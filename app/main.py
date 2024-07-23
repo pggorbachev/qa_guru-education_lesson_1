329c155fa3e719e177f14075ee64b1f20b48182c
Pavel Gorbachev <pavelgorbachev7551@gmail.com>
2024-07-23 17:04:52 +0300

    create fastAPI microservice and add tests for this app

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from users_data.users import users_data
+
+app = FastAPI()
+
+
+@app.get("/api/user/{user_id}")
+async def get_user(user_id: int):
+    user = users_data.get(user_id)
+    if user:
+        return JSONResponse(content={"data": user, "support": {
+            "url": "https://reqres.in/#support-heading",
+            "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+        }})
+    else:
+        return JSONResponse(status_code=404, content={"error": "User not found"})
---
+++ tests/test_api_positive.py
@@ -0,0 +1,33 @@
+import requests
+import pytest
+
+BASE_URL = "http://localhost:8000/api/user"
+
+def test_get_user_1():
+    response = requests.get(f"{BASE_URL}/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"]["id"] == 1
+    assert data["data"]["email"] == "george.bluth@reqres.in"
+    assert data["data"]["first_name"] == "George"
+    assert data["data"]["last_name"] == "Bluth"
+
+
+def test_get_user_2():
+    response = requests.get(f"{BASE_URL}/2")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"]["id"] == 2
+    assert data["data"]["email"] == "janet.weaver@reqres.in"
+    assert data["data"]["first_name"] == "Janet"
+    assert data["data"]["last_name"] == "Weaver"
+
+def test_get_user_not_found():
+    response = requests.get(f"{BASE_URL}/999")
+    assert response.status_code == 404
+    data = response.json()
+    assert data["error"] == "User not found"
+
+def test_invalid_user_id():
+    response = requests.get(f"{BASE_URL}/abc")
+    assert response.status_code == 422
